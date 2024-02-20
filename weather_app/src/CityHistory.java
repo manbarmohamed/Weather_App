4da6d83e7b9c03af8d8d506a35abb185015530d5
simo manbar <you@example.com>
2024-02-20 15:06:14 +0100

    creation des files de projet

---
+++ weather_app/src/CityHistory.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+
+public class CityHistory {
+    private int historicalDataId;
+    private int cityId;
+
+    private LocalDate eventDate;
+    private int temperature;
+    public CityHistory(int historicalDataId, int cityId, LocalDate eventDate, int temperature) {
+        this.historicalDataId = historicalDataId;
+        this.cityId = cityId;
+        this.eventDate = eventDate;
+        this.temperature = temperature;
+    }
+
+    public int getHistoricalDataId() {
+        return historicalDataId;
+    }
+
+    public void setHistoricalDataId(int historicalDataId) {
+        this.historicalDataId = historicalDataId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+}
